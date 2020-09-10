0a8ad5a4e1eee8e990c99fdb1b7398b8bc5cc906
shaistamadad <58788471+shaistamadad@users.noreply.github.com>
2020-09-10 00:52:32 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+	public static void main(String[] args) {
+		//I quite like this song. It's sung by Nusrat Fateh Ali Khan 
+		System.out.println("HelloWorld");
+		System.out.println("How Happy is the Blameless Vestal's Lot!" +
+			               "\nThe World Forgetting, By the World Forgot" +
+			               "\nEternal Sunshine of the Spotless Mind"+
+			               "\nEach Prayer Accepted and Each Wish Denied");
+	}
+}
